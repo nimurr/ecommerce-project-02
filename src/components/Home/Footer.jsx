4b423b19ad7decc40df0042f76a8af463e11deda
nimurr <nimurnerob404@gmail.com>
2023-09-21 05:42:55 -0600

    update footer

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer className="footer p-10 bg-base-200 text-base-content">
                 <aside>
                     <NavLink to='/' className="text-4xl text-blue-600 font-semibold cursor-pointer">E-mart</NavLink>
-                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    <p>E-mart Industries Ltd.<br />Providing reliable tech since 1992</p>
                 </aside>
                 <nav>
                     <header className="footer-title">Services</header>
