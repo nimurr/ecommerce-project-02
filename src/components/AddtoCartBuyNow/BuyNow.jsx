c5b26084d3c638d9df95f1d277cda88a868c2fce
nimurr <nimurnerob404@gmail.com>
2023-09-21 05:39:04 -0600

    add to toast in buy now btn

--- src/components/AddtoCartBuyNow/BuyNow.jsx
+++ src/components/AddtoCartBuyNow/BuyNow.jsx
@@ -13,7 +13,7 @@ export default function BuyNow() {
                 <img className="w-20" src={product.image} alt="" />
                 <div className="text-center">
                     <h3 className="text-4xl font-semibold ">${product.price}</h3>
-                    <h2 className="text-xl font-semibold">{product.title}</h2>
+                    <h2 className="text-xl font-semibold">{product.title.slice(0,20)}</h2>
                 </div>
                 <button onClick={handlebuyNowBtn} className="bg-blue-600 text-white px-4 py-2 rounded mt-2">Pay Now</button>
             </div>
